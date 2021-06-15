789ed001751a5f24190610d3b7774e04b57a02b6
Plueksaporn.K <plueksaporn.kattiyawong@scb.co.th>
2021-06-15 11:32:40 +0700

    workshop3-lab2 navigation and pass parameter to another screen

---
+++ lib/secondPage.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatelessWidget {
+  Color rederColor(String param) {
+    switch (param) {
+      case 'Red':
+        return Colors.red;
+        break;
+      case 'Green':
+        return Colors.green;
+        break;
+      default:
+        return Colors.yellow;
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final param = ModalRoute.of(context)?.settings?.arguments as String;
+    print(param);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(param),
+      ),
+      body: Container(
+        color: rederColor(param),
+        alignment: Alignment.center,
+        child: Text(
+          param,
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
